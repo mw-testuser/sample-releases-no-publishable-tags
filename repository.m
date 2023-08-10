b4586f63ccef3d384d8cc4f1a5cac5479ee7ae76
test-testperson <98484229+test-testperson@users.noreply.github.com>
2023-08-09 21:30:09 -0400

    Add repository function

---
+++ repository.m
@@ -0,0 +1,5 @@
+function metadata = repository
+metadata.owner = "test-testperson";
+metadata.name = "sample-releases-no-publishable-tags";
+metadata.url = "https://github.com/test-testperson/sample-releases-no-publishable-tags";
+end
